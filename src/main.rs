39473e99a73a9d75a404231d95f8e49c0bc43820
Kisaragi Marine <kisaragi.effective@gmail.com>
2022-08-03 05:54:54 +0900

    feat: proper impl

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
+use std::fmt::Display;
 use std::path::PathBuf;
 use clap::Parser;
 use clap::Subcommand;
+use fern::colors::ColoredLevelConfig;
+use log::{LevelFilter, trace};
+use strum::EnumString;
 
 #[derive(Parser)]
 struct ToolChainArgs {
     #[clap(long)]
     log_level: ToolChainLogLevel,
+    #[clap(long)]
+    color_policy: ColorPolicy,
+    #[clap(long)]
+    log_file: Option<PathBuf>,
     #[clap(subcommand)]
     sub_command: ToolChainSubCommand,
 }
@@ -32,6 +40,75 @@ enum ToolChainSubCommand {
     },
 }
 
+#[derive(EnumString, Eq, PartialEq, Copy, Clone)]
+#[strum(serialize_all = "camelCase")]
+enum ColorPolicy {
+    Always,
+    Auto,
+    Never,
+}
+
+impl ColorPolicy {
+    fn determine(self, stream: atty::Stream) -> bool {
+        match self {
+            ColorPolicy::Always => true,
+            ColorPolicy::Auto => atty::is(stream),
+            ColorPolicy::Never => false,
+        }
+    }
+}
+
+#[derive(EnumString, Eq, PartialEq, Copy, Clone)]
+#[strum(serialize_all = "camelCase")]
+enum ToolChainLogLevel {
+    Off,
+    Error,
+    Warning,
+    Info,
+    Debug,
+    Trace,
+}
+
+fn setup_logger(log_level: ToolChainLogLevel, do_color: bool, output_file: Option<PathBuf>) -> Result<(), fern::InitError> {
+    let color = ColoredLevelConfig::new();
+
+    let mut x = fern::Dispatch::new()
+        .format(move |out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                if do_color {
+                    Box::new(color.color(record.level())) as Box<dyn Display>
+                } else {
+                    Box::new(record.level()) as Box<dyn Display>
+                },
+                message
+            ))
+        })
+        .level(match log_level {
+            ToolChainLogLevel::Off => LevelFilter::Off,
+            ToolChainLogLevel::Error => LevelFilter::Error,
+            ToolChainLogLevel::Warning => LevelFilter::Warn,
+            ToolChainLogLevel::Info => LevelFilter::Info,
+            ToolChainLogLevel::Debug => LevelFilter::Debug,
+            ToolChainLogLevel::Trace => LevelFilter::Trace,
+        })
+        .chain(std::io::stderr());
+
+    if let Some(log_file) = output_file {
+        x = x.chain(fern::log_file("output.log")?);
+    }
+
+    x.apply()?;
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: ToolChainArgs = ToolChainArgs::parse();
+    setup_logger(args.log_level, args.color_policy.determine(atty::Stream::Stdout), args.log_file)
+        .unwrap_or_default();
+    trace!("Hello!");
+
+    trace!("Bye!");
 }
