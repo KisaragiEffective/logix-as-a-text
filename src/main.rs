2d9868918ff4e176068fd2e314aa6066c928b8d9
Kisaragi Marine <kisaragi.effective@gmail.com>
2022-08-03 05:33:01 +0900

    feat: prepare library and args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::path::PathBuf;
+use clap::Parser;
+use clap::Subcommand;
+
+#[derive(Parser)]
+struct ToolChainArgs {
+    #[clap(long)]
+    log_level: ToolChainLogLevel,
+    #[clap(subcommand)]
+    sub_command: ToolChainSubCommand,
+}
+
+#[derive(Subcommand)]
+enum ToolChainSubCommand {
+    Compress {
+        path: PathBuf,
+    },
+    Decompress {
+        path: PathBuf,
+    },
+    Compile {
+        source_file: PathBuf,
+    },
+    GenerateStub {
+        json_file: PathBuf,
+    },
+    DumpJson {
+        json_file: PathBuf,
+    },
+    DumpAst {
+        source_file: PathBuf,
+    },
+}
+
 fn main() {
     println!("Hello, world!");
 }
