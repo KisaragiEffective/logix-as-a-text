33ceddfaf245fe25ff2532f01502f34c9b16dff8
Kisaragi Marine <kisaragi.effective@gmail.com>
2022-08-07 20:18:42 +0900

    feat: 文字列リテラルのサポート

--- src/compiler/lexer.rs
+++ src/compiler/lexer.rs
@@ -1,6 +1,6 @@
 use std::cell::Cell;
 
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, bail, Result};
 
 static KEYWORDS: [&str; 10] = ["if", "then", "else", "elseif", "end", "endif", "while", "wend", "for", "match"];
 pub struct Lexer {
@@ -127,6 +127,14 @@ impl Lexer {
                     Token::SymBang
                 }
             },
+            '"' => {
+                self.advance();
+                let scanned_content = self.scan_string_literal().unwrap();
+                assert_eq!(self.consume_char().unwrap(), '"', "The string literal is not terminated");
+                Token::StringLiteral {
+                    content: scanned_content,
+                }
+            },
             c if c.is_ascii_digit() => self.scan_digits().expect("oops"),
             c if c.is_ascii_lowercase() => {
                 let scan_result = self.scan_lowers().expect("oops");
@@ -201,6 +209,126 @@ impl Lexer {
         Ok(buf)
     }
 
+    fn scan_string_literal(&self) -> Result<String> {
+        self.advance();
+        let mut buf = String::new();
+        let mut in_escape = false;
+        loop {
+            if in_escape {
+                match self.current_char().expect("oops") {
+                    '"' => {
+                        buf.push('"');
+                        self.consume_char()?;
+                        in_escape = false;
+                        continue
+                    }
+                    '\\' => {
+                        buf.push('\\');
+                        self.consume_char()?;
+                        in_escape = false;
+                        continue
+                    }
+                    'a' => {
+                        buf.push('\x07');
+                        self.consume_char()?;
+                        in_escape = false;
+                        continue
+                    }
+                    'b' => {
+                        buf.push('\x08');
+                        self.consume_char()?;
+                        in_escape = false;
+                        continue
+                    }
+                    't' => {
+                        buf.push('\t');
+                        self.consume_char()?;
+                        in_escape = false;
+                        continue
+                    }
+                    'n' => {
+                        buf.push('\n');
+                        self.consume_char()?;
+                        in_escape = false;
+                        continue
+                    }
+                    'v' => {
+                        buf.push('\x0b');
+                        self.consume_char()?;
+                        in_escape = false;
+                        continue
+                    }
+                    'f' => {
+                        buf.push('\x0c');
+                        self.consume_char()?;
+                        in_escape = false;
+                        continue
+                    }
+                    'r' => {
+                        buf.push('\x0d');
+                        self.consume_char()?;
+                        in_escape = false;
+                        continue
+                    }
+                    'u' => {
+                        self.consume_char()?;
+                        // TODO: support unicode codepoint sequence: \u{ 3015 3028 3033 }
+                        let mut codepoint: u16 = 0;
+                        for _ in 0..=3 {
+                            let or_value = match self.current_char()
+                                .expect("There must be more characters to recognize Unicode escape sequence") {
+                                '0' => 0,
+                                '1' => 1,
+                                '2' => 2,
+                                '3' => 3,
+                                '4' => 4,
+                                '5' => 5,
+                                '6' => 6,
+                                '7' => 7,
+                                '8' => 8,
+                                '9' => 9,
+                                'A' | 'a' => 0xA,
+                                'B' | 'b' => 0xB,
+                                'C' | 'c' => 0xC,
+                                'D' | 'd' => 0xD,
+                                'E' | 'e' => 0xE,
+                                'F' | 'f' => 0xF,
+                                '"' => bail!("An Unicode escape sequence must have four hexadecimal codepoint, but there's no codepoint anymore."),
+                                other_char => bail!("An Unicode escape sequence must have four hexadecimal codepoint, but there's other character that is not valid a codepoint character."),
+                            };
+                            codepoint = (codepoint << 4) | or_value;
+                            self.consume_char().unwrap();
+                        }
+                        let surrogate_code_point_range = 0xD800..=0xDFFF;
+                        if surrogate_code_point_range.contains(&codepoint) {
+                            bail!("This codepoint ({codepoint:x}) is invalid Unicode scalar value. The codepoint of unicode escape sequence must not be a surrogate code points.\
+                            Note: An \"Unicode scalar value\" is defined as \"any Unicode code point except high-surrogate and low-surrogate code points.\" in the Unicode glossary.\
+                                  To see full definition, please see https://www.unicode.org/glossary/#unicode_scalar_value")
+                        }
+                        buf.push(char::from_u32(codepoint as u32).unwrap());
+                        in_escape = false;
+                        continue
+                    }
+                    other_char => {
+                        bail!("The char ({other_char}) is not an acceptable char as escape-sequence.")
+                    }
+                }
+            } else {
+                match self.current_char().expect("oops") {
+                    '"' => break,
+                    '\\' => {
+                        in_escape = true;
+                        continue
+                    }
+                    '\n' => bail!("String literal can not contain newline literally. To script newline, please escape as \"\\n\"."),
+                    other_char => buf.push(other_char),
+                }
+            }
+        }
+
+        Ok(buf)
+    }
+
     pub fn peek(&self) -> Token {
         let current_index = self.index.get();
         let token = self.next();
@@ -253,6 +381,9 @@ pub enum Token {
         char: char,
     },
     Comment {
+        content: String,
+    },
+    StringLiteral {
         content: String,
     },
     EndOfFile,
